bc726c7bd1a2c910f07094a6685a082df0724b86
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-04 17:05:24 +0800

    Added crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,16 @@ export default function Home() {
   return (
     <>
       <Head>
+        {/* =========HEADER FONT=========== */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&display=swap"
           rel="stylesheet"
         />
-        {/* ==================== */}
+        {/* =========TEXT FONT=========== */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Merienda&display=swap"
           rel="stylesheet"
