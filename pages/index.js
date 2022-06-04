ccab604efac885f32bef57cc3fa62afbd3430a40
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-04 17:03:40 +0800

    Added custom fonts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
+import Head from "next/head";
+import TopNav from "../components/TopNav";
+
 export default function Home() {
   return (
     <>
-      <h1>POTANGINA MO!</h1>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&display=swap"
+          rel="stylesheet"
+        />
+        {/* ==================== */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merienda&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      {/* ========================== */}
+      <TopNav />
     </>
   );
 }
