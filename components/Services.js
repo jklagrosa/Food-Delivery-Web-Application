0745963c6cd392ebdf394bcef2ada4f2b902843d
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-05 16:44:23 +0800

    Added services component

---
+++ components/Services.js
@@ -0,0 +1,11 @@
+const Services = () => {
+  return (
+    <>
+      Veniam voluptate duis tempor aliqua tempor enim. Eu non sint excepteur
+      cupidatat non voluptate. Sunt ipsum aute elit ex non cupidatat Lorem ex
+      fugiat proident. Consequat laboris do eiusmod consequat eu duis ex.
+    </>
+  );
+};
+
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import TopNav from "../components/TopNav";
 import Navigation from "../components/Nav";
 import Hero from "../components/Hero";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <TopNav />
       <Navigation />
       <Hero />
+      <Services />
     </>
   );
 }
