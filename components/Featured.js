4d31b89629a83d7d111f74876c184adad26f5e30
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-06 20:18:07 +0800

    Testing the application

--- components/Featured.js
+++ components/Featured.js
@@ -6,7 +6,13 @@ const Featured = () => {
       velit. Laboris dolore aute nisi occaecat qui esse aliquip consequat nulla
       nostrud minim et ad adipisicing. Velit est deserunt et proident
       consectetur nostrud deserunt. Aliquip occaecat culpa minim consectetur. Id
-      excepteur aute id Lorem deserunt irure.
+      excepteur aute id Lorem deserunt irure. Cupidatat officia excepteur
+      deserunt tempor duis proident amet quis reprehenderit do. Ea aute aliquip
+      cillum proident cupidatat reprehenderit nostrud cupidatat consectetur
+      exercitation ipsum laborum. Anim quis qui occaecat eu ad velit id ad sit
+      ut veniam ipsum occaecat exercitation. In cillum laborum magna amet
+      ullamco eiusmod duis laborum et amet eiusmod ut aliqua. Nisi amet fugiat
+      voluptate consectetur sit ad reprehenderit sit nisi mollit ut est eiusmod.
     </>
   );
 };
