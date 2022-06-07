3831d7a77a57e73a220893ec5f4a706886ecf750
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-07 14:37:42 +0800

    Added whyUs component

---
+++ components/WhyUs.js
@@ -0,0 +1,12 @@
+const WhyUs = () => {
+  return (
+    <>
+      Magna sint tempor qui laborum minim commodo veniam aliqua consequat
+      commodo. Dolor eu aliqua duis magna nisi dolor magna in. Dolore ut
+      reprehenderit reprehenderit Lorem ea velit sit aute esse. Esse do non
+      consequat voluptate esse sit dolore proident incididunt dolore mollit et.
+    </>
+  );
+};
+
+export default WhyUs;
