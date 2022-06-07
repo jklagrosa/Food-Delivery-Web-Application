d7809218e0962560b0ccd37755e7240f21982ed1
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-07 20:12:51 +0800

    Added breakpoints in footer component

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
       <div className={styles.Wrapper}>
         <Container fluid="lg" className="p-0">
-          <Row className="gy-5 gx-3">
+          <Row className="gy-4 gx-3">
             <Col xs={12} md={4}>
               <div className={styles._footer_box}>
                 <h3>LutongBahay</h3>
@@ -32,6 +32,7 @@ const Footer = () => {
                 >
                   LinkedIn
                 </a>
+                <br></br>
                 <a
                   href="https://github.com/jklagrosa"
                   target="_blank"
@@ -39,6 +40,7 @@ const Footer = () => {
                 >
                   Github
                 </a>
+                <br></br>
                 <a
                   href="mailto:jklagrosa.dev@gmail.com"
                   target="_blank"
@@ -46,6 +48,21 @@ const Footer = () => {
                 >
                   Gmail
                 </a>
+              </div>
+            </Col>
+
+            <Col xs={12} md={4}>
+              <div className={styles._footer_box}>
+                <h6 className={styles._footer_box_h6}>Tech Used</h6>
+                <p className={styles._footer_box_tech_used}>HTML5</p>
+                <p className={styles._footer_box_tech_used}>CSS3</p>
+                <p className={styles._footer_box_tech_used}>Sass</p>
+                <p className={styles._footer_box_tech_used}>Bootstrap</p>
+                <p className={styles._footer_box_tech_used}>JavaScript</p>
+                <p className={styles._footer_box_tech_used}>React JS</p>
+                <p className={styles._footer_box_tech_used}>Next JS</p>
+                <p className={styles._footer_box_tech_used}>Redux Toolkit</p>
+                <p className={styles._footer_box_tech_used}>MongoDB</p>
               </div>
             </Col>
           </Row>
