5780c66b28fd2eeacdea12054ad29c681c037b42
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-07 14:47:59 +0800

    Added parallax image

--- components/WhyUs.js
+++ components/WhyUs.js
@@ -1,10 +1,13 @@
+import styles from "../styles/WhyUs.module.scss";
+
 const WhyUs = () => {
   return (
     <>
-      Magna sint tempor qui laborum minim commodo veniam aliqua consequat
-      commodo. Dolor eu aliqua duis magna nisi dolor magna in. Dolore ut
-      reprehenderit reprehenderit Lorem ea velit sit aute esse. Esse do non
-      consequat voluptate esse sit dolore proident incididunt dolore mollit et.
+      <div className={styles.Wrapper}>
+        <h1>
+          Why <span>Us</span>
+        </h1>
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Services from "../components/Services";
 import TodaySpecial from "../components/TS";
 import Featured from "../components/Featured";
 import Promo from "../components/Promo";
+import WhyUs from "../components/WhyUs";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <TodaySpecial />
       <Featured />
       <Promo />
+      <WhyUs />
     </>
   );
 }
