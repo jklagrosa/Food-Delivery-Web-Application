f7085d49168d90ab76145bc8ce5729c14a720184
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-07 15:52:00 +0800

    Added faq component

---
+++ components/FAQ.js
@@ -0,0 +1,12 @@
+const FAQ = () => {
+  return (
+    <>
+      Non esse eiusmod cillum voluptate laborum. Laborum nisi velit ipsum
+      voluptate veniam magna enim consequat occaecat sunt sit. Enim cupidatat
+      commodo Lorem magna qui amet enim. Sint dolor mollit amet magna cillum
+      nulla sunt sint cupidatat enim nostrud nostrud.
+    </>
+  );
+};
+
+export default FAQ;
