1b18007a976a1f0d8040ae349987dc2eb7eb1b00
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-07 19:30:54 +0800

    Added social links to the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,56 @@
+import styles from "../styles/Footer.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
   return (
     <>
-      Mollit nostrud occaecat aute in tempor nisi amet. Aute cillum ea sunt duis
-      cillum eu pariatur ad. Ipsum laboris ipsum consequat laborum.
+      <div className={styles.Wrapper}>
+        <Container fluid="lg" className="p-0">
+          <Row className="gy-5 gx-3">
+            <Col xs={12} md={4}>
+              <div className={styles._footer_box}>
+                <h3>LutongBahay</h3>
+                <p className={styles._footer_box_desc}>
+                  If you want to try this web application, you can login as Demo
+                  user. Click the login button to redirect to login page and
+                  then click the{" "}
+                  <code>
+                    <q>Login as demo user</q>
+                  </code>{" "}
+                  button.
+                </p>
+              </div>
+            </Col>
+
+            <Col xs={12} md={4}>
+              <div className={styles._footer_box}>
+                <h6 className={styles._footer_box_h6}>Contact the dev.</h6>
+                <a
+                  href="https://www.linkedin.com/in/jklagrosa/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  href="https://github.com/jklagrosa"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Github
+                </a>
+                <a
+                  href="mailto:jklagrosa.dev@gmail.com"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Gmail
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
