0d88ebd1e70453db0c7eadcd2fa2d7bad1770017
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-07 20:40:49 +0800

    Added copyright module.scss

--- components/Copyright.js
+++ components/Copyright.js
@@ -1,10 +1,17 @@
+import styles from "../styles/Copyright.module.scss";
+import { Container } from "react-bootstrap";
+
 const Copyright = () => {
   return (
     <>
-      Cupidatat officia dolor aliquip aliqua ut eu eu ut anim sunt. Magna dolor
-      velit dolore cupidatat id dolore duis cillum velit tempor cupidatat ut ut
-      tempor. Ullamco irure dolor sint esse. Tempor mollit voluptate excepteur
-      tempor.
+      <div className={styles.Wrapper}>
+        Nulla officia non eiusmod quis eu. Exercitation deserunt ullamco labore
+        adipisicing minim culpa fugiat. Mollit anim enim exercitation culpa
+        fugiat nostrud officia. Pariatur officia veniam enim voluptate culpa
+        duis aute duis id quis voluptate. Enim dolor amet aliquip esse minim
+        anim dolor proident est. Aute eiusmod ullamco veniam pariatur ut ipsum.
+        Ut occaecat aliquip cillum deserunt.
+      </div>
     </>
   );
 };
