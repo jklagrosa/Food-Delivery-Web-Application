027f364c22f82dc1d46398192a401b2cf1314452
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-09 16:29:23 +0800

    Added product page

---
+++ pages/product/index.js
@@ -0,0 +1,11 @@
+const Product = () => {
+  return (
+    <>
+      Fugiat et in sunt cillum qui dolore. Duis nisi mollit eiusmod minim ad
+      voluptate in amet id deserunt Lorem officia deserunt laboris. Et qui minim
+      laboris ullamco nulla do dolor.
+    </>
+  );
+};
+
+export default Product;
