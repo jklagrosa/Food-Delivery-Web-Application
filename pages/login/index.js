70759c4b61dbb2c0e012a285b7cf2fde83366121
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-11 16:53:34 +0800

    Added login.module.scss

---
+++ pages/login/index.js
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <>
+      Ex in ipsum non deserunt mollit duis mollit veniam amet velit aliqua
+      commodo labore ut. Sint minim officia enim in adipisicing in culpa Lorem
+      aute tempor proident. Ut eu commodo adipisicing voluptate reprehenderit
+      dolor anim mollit fugiat adipisicing. Commodo sunt laborum velit tempor
+      nostrud est est irure et ut. Veniam duis do labore nisi reprehenderit
+      occaecat ea tempor sit.
+    </>
+  );
+};
+
+export default Login;
