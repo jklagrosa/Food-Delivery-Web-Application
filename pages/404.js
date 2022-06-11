6944a1dcde11e32dcaa74ca55f2bcbe65dd3caaf
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-11 18:19:25 +0800

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <>
+      Reprehenderit ad veniam do veniam. Ullamco irure qui culpa ea commodo
+      deserunt mollit veniam ea in nulla officia pariatur. Excepteur labore ex
+      magna eu eiusmod. Fugiat pariatur ullamco eiusmod elit aliquip sunt Lorem
+      anim eiusmod dolore est aute eu cupidatat. Et ea dolore sint ullamco.
+    </>
+  );
+};
+
+export default NotFound;
