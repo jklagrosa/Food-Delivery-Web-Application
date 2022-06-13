5ac1f79c5d5b4bd220c2f58fb5cb9d043136b1ed
jklagrosa <jklagrosa.dev@gmail.com>
2022-06-13 13:30:34 +0800

    Displaying data in dynamic page

--- components/TS.js
+++ components/TS.js
@@ -26,6 +26,15 @@ const TodaySpecial = () => {
     }
   }, [todays_special]);
 
+  const handleSeeMore = (id) => {
+    if (id) {
+      router.push({
+        pathname: "/product/[id]",
+        query: { id: id },
+      });
+    }
+  };
+
   return (
     <>
       <div className={styles.Wrapper}>
@@ -45,6 +54,7 @@ const TodaySpecial = () => {
                           src={`/dish/${res.img}`}
                           alt={res.title}
                           id={styles._ts_card_img}
+                          onClick={() => handleSeeMore(res._id)}
                         />
                       </abbr>
                       <Card.Body>
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -15,171 +15,262 @@ import { AiOutlineStar } from "react-icons/ai";
 import { BiDish } from "react-icons/bi";
 import { RiPriceTag3Line } from "react-icons/ri";
 
-const ProductID = () => {
+import dbConnection from "../../utils/conn";
+import Dish from "../../models/dish";
+
+export async function getStaticPaths() {
+  await dbConnection();
+  const get_id = await Dish.find({});
+  const paths = get_id.map((x) => {
+    return {
+      params: { id: `${x._id}` },
+    };
+  });
+
+  if (!get_id) {
+    return {
+      paths: [],
+      fallback: false,
+    };
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  await dbConnection();
+  const { params } = context;
+  const find_products = await Dish.findOne({ _id: `${params.id}` });
+  if (!find_products) {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+
+  console.log(find_products);
+
+  return {
+    props: {
+      data: JSON.stringify(find_products),
+    },
+  };
+}
+
+const ProductID = ({ data }) => {
   const router = useRouter();
+  const [product, setProduct] = useState(null);
+  const [HAS_PRODUCT, SET_HAS_PRODUCT] = useState(true);
+  const parsed_product = data ? JSON.parse(data) : false;
+
+  useEffect(() => {
+    if (!parsed_product) {
+      setProduct(null);
+      SET_HAS_PRODUCT(false);
+    } else {
+      setProduct(parsed_product);
+      SET_HAS_PRODUCT(true);
+    }
+  }, []);
+
   return (
     <>
       <TopNav />
       <Nav />
       <div className={styles.Wrapper}>
         <Container fluid="lg" className="p-0">
-          <Breadcrumb>
-            <Breadcrumb.Item
-              onClick={() => router.push("/")}
-              className={styles._product_bread_crumbs}
-            >
-              Home
-            </Breadcrumb.Item>
-            <Breadcrumb.Item active className={styles._product_bread_crumbs}>
-              Product
-            </Breadcrumb.Item>
-            <Breadcrumb.Item active className={styles._product_bread_crumbs}>
-              Data
-            </Breadcrumb.Item>
-          </Breadcrumb>
-          <Row className="gy-0 gx-4" id={styles._product_row_wrapper}>
-            <Col md={12} lg={8}>
-              <div className={styles._product_display}>
-                <img src="/dish/d6.jpg" alt="p1.jpg" />
+          {HAS_PRODUCT && (
+            <>
+              <Breadcrumb>
+                <Breadcrumb.Item
+                  onClick={() => router.push("/")}
+                  className={styles._product_bread_crumbs}
+                >
+                  Home
+                </Breadcrumb.Item>
+                <Breadcrumb.Item
+                  active
+                  className={styles._product_bread_crumbs}
+                >
+                  Product
+                </Breadcrumb.Item>
+                <Breadcrumb.Item
+                  active
+                  className={styles._product_bread_crumbs}
+                >
+                  Data
+                </Breadcrumb.Item>
+              </Breadcrumb>
+              <Row className="gy-0 gx-4" id={styles._product_row_wrapper}>
+                <Col md={12} lg={8}>
+                  <div className={styles._product_display}>
+                    <img src="/dish/d6.jpg" alt="p1.jpg" />
 
-                <div className={styles._product_cart_n_wish}>
-                  <abbr title="Add to your Wishlist" style={{ all: "unset" }}>
-                    <button>
-                      <BsFillSuitHeartFill
-                        className={styles._product_cart_n_wish_ICON}
-                      />{" "}
-                      Add to Wishlist
-                    </button>
-                  </abbr>
+                    <div className={styles._product_cart_n_wish}>
+                      <abbr
+                        title="Add to your Wishlist"
+                        style={{ all: "unset" }}
+                      >
+                        <button>
+                          <BsFillSuitHeartFill
+                            className={styles._product_cart_n_wish_ICON}
+                          />{" "}
+                          Add to Wishlist
+                        </button>
+                      </abbr>
 
-                  <abbr title="Add to your Cart" style={{ all: "unset" }}>
-                    <button>
-                      <BsFillCartFill
-                        className={styles._product_cart_n_wish_ICON}
-                      />{" "}
-                      Add to Cart
-                    </button>
-                  </abbr>
-                </div>
+                      <abbr title="Add to your Cart" style={{ all: "unset" }}>
+                        <button>
+                          <BsFillCartFill
+                            className={styles._product_cart_n_wish_ICON}
+                          />{" "}
+                          Add to Cart
+                        </button>
+                      </abbr>
+                    </div>
 
-                <h1>Adobo Pork</h1>
+                    <h1>Adobo Pork</h1>
 
-                <abbr title="Cuisine" style={{ all: "unset" }}>
-                  <span className={styles._product_span}>
-                    <BsGlobe2 className={styles._product_icon} /> Filipino
-                  </span>
-                </abbr>
-                {/*  */}
-                <abbr title="Ratings" style={{ all: "unset" }}>
-                  <span className={styles._product_span}>
-                    <AiOutlineStar className={styles._product_icon} /> 4.5/5
-                  </span>
-                </abbr>
-                {/*  */}
-                <abbr title="Course" style={{ all: "unset" }}>
-                  <span className={styles._product_span}>
-                    <BiDish className={styles._product_icon} /> Main Course
-                  </span>
-                </abbr>
+                    <abbr title="Cuisine" style={{ all: "unset" }}>
+                      <span className={styles._product_span}>
+                        <BsGlobe2 className={styles._product_icon} /> Filipino
+                      </span>
+                    </abbr>
+                    {/*  */}
+                    <abbr title="Ratings" style={{ all: "unset" }}>
+                      <span className={styles._product_span}>
+                        <AiOutlineStar className={styles._product_icon} /> 4.5/5
+                      </span>
+                    </abbr>
+                    {/*  */}
+                    <abbr title="Course" style={{ all: "unset" }}>
+                      <span className={styles._product_span}>
+                        <BiDish className={styles._product_icon} /> Main Course
+                      </span>
+                    </abbr>
 
-                <h2 className={styles._product_display_price}>₱300</h2>
+                    <h2 className={styles._product_display_price}>₱300</h2>
 
-                <p className={styles._product_desc}>
-                  Fugiat magna labore duis Lorem duis. Magna mollit consectetur
-                  proident exercitation qui aute dolor excepteur culpa. Lorem
-                  elit occaecat excepteur magna amet eiusmod dolor deserunt amet
-                  cillum sunt dolore. Deserunt sit consequat et cupidatat id.
-                </p>
+                    <p className={styles._product_desc}>
+                      Fugiat magna labore duis Lorem duis. Magna mollit
+                      consectetur proident exercitation qui aute dolor excepteur
+                      culpa. Lorem elit occaecat excepteur magna amet eiusmod
+                      dolor deserunt amet cillum sunt dolore. Deserunt sit
+                      consequat et cupidatat id.
+                    </p>
 
-                <p className={styles._product_desc}>
-                  Fugiat magna labore duis Lorem duis. Magna mollit consectetur
-                  proident exercitation qui aute dolor excepteur culpa. Lorem
-                  elit occaecat excepteur magna amet eiusmod dolor deserunt amet
-                  cillum sunt dolore. Deserunt sit consequat et cupidatat id.
-                </p>
+                    <p className={styles._product_desc}>
+                      Fugiat magna labore duis Lorem duis. Magna mollit
+                      consectetur proident exercitation qui aute dolor excepteur
+                      culpa. Lorem elit occaecat excepteur magna amet eiusmod
+                      dolor deserunt amet cillum sunt dolore. Deserunt sit
+                      consequat et cupidatat id.
+                    </p>
 
-                <h2 className={styles._product_ingredient_h2}>Ingredients</h2>
-                <ul>
-                  <li>2 lbs. pork shoulder cubed</li>
-                  <li>4 bay leaves</li>
-                  <li>½ cup soy sauce</li>
-                  <li>½ cup white vinegar</li>
-                  <li>7 ½ ounces lemon lime soda</li>
-                  <li>2 tablespoons oyster sauce</li>
-                  <li>2 teaspoons peppercorn crushed</li>
-                  <li>2 heads garlic</li>
-                  <li>1 onion</li>
-                  <li>¼ cup cooking oil</li>
-                </ul>
+                    <h2 className={styles._product_ingredient_h2}>
+                      Ingredients
+                    </h2>
+                    <ul>
+                      <li>2 lbs. pork shoulder cubed</li>
+                      <li>4 bay leaves</li>
+                      <li>½ cup soy sauce</li>
+                      <li>½ cup white vinegar</li>
+                      <li>7 ½ ounces lemon lime soda</li>
+                      <li>2 tablespoons oyster sauce</li>
+                      <li>2 teaspoons peppercorn crushed</li>
+                      <li>2 heads garlic</li>
+                      <li>1 onion</li>
+                      <li>¼ cup cooking oil</li>
+                    </ul>
 
-                <h2 className={styles._product_ingredient_h2}>Nutrition</h2>
-                <ul>
-                  <li>Calories: 528kcal</li>
-                  <li>Carbohydrates: 13g</li>
-                  <li>Protein: 48g</li>
-                  <li>Fat: 30g</li>
-                  <li>Saturated Fat: 7g</li>
-                  <li>Polyunsaturated Fat: 6g</li>
-                </ul>
+                    <h2 className={styles._product_ingredient_h2}>Nutrition</h2>
+                    <ul>
+                      <li>Calories: 528kcal</li>
+                      <li>Carbohydrates: 13g</li>
+                      <li>Protein: 48g</li>
+                      <li>Fat: 30g</li>
+                      <li>Saturated Fat: 7g</li>
+                      <li>Polyunsaturated Fat: 6g</li>
+                    </ul>
 
-                {/* <hr className={styles._product_divider} /> */}
-              </div>
-            </Col>
-            <Col md={12} lg={4} id={styles._product_hide_on_SMALL_DEVICE}>
-              <div className={styles._product_you_may_like}>
-                <h2>You may like</h2>
-                <div className={styles._product_you_may_like_box}>
-                  <h5 className={styles._product_title}>Adobo Manok</h5>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <BsGlobe2 />
-                    Filipino
-                  </span>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <AiOutlineStar />
-                    4.5/5
-                  </span>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <BiDish />
-                    Side Dish
-                  </span>
-                </div>
-                {/* ================== */}
-                <div className={styles._product_you_may_like_box}>
-                  <h5 className={styles._product_title}>Adobo Manok</h5>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <BsGlobe2 />
-                    Filipino
-                  </span>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <AiOutlineStar />
-                    4.5/5
-                  </span>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <BiDish />
-                    Side Dish
-                  </span>
-                </div>
+                    {/* <hr className={styles._product_divider} /> */}
+                  </div>
+                </Col>
+                <Col md={12} lg={4} id={styles._product_hide_on_SMALL_DEVICE}>
+                  <div className={styles._product_you_may_like}>
+                    <h2>You may like</h2>
+                    <div className={styles._product_you_may_like_box}>
+                      <h5 className={styles._product_title}>Adobo Manok</h5>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <BsGlobe2 />
+                        Filipino
+                      </span>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <AiOutlineStar />
+                        4.5/5
+                      </span>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <BiDish />
+                        Side Dish
+                      </span>
+                    </div>
+                    {/* ================== */}
+                    <div className={styles._product_you_may_like_box}>
+                      <h5 className={styles._product_title}>Adobo Manok</h5>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <BsGlobe2 />
+                        Filipino
+                      </span>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <AiOutlineStar />
+                        4.5/5
+                      </span>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <BiDish />
+                        Side Dish
+                      </span>
+                    </div>
 
-                {/* ======================== */}
+                    {/* ======================== */}
 
-                <div className={styles._product_you_may_like_box}>
-                  <h5 className={styles._product_title}>Adobo Manok</h5>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <BsGlobe2 />
-                    Filipino
-                  </span>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <AiOutlineStar />
-                    4.5/5
-                  </span>
-                  <span className={styles._product_you_may_like_SPAN}>
-                    <BiDish />
-                    Side Dish
-                  </span>
-                </div>
-              </div>
-            </Col>
-          </Row>
+                    <div className={styles._product_you_may_like_box}>
+                      <h5 className={styles._product_title}>Adobo Manok</h5>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <BsGlobe2 />
+                        Filipino
+                      </span>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <AiOutlineStar />
+                        4.5/5
+                      </span>
+                      <span className={styles._product_you_may_like_SPAN}>
+                        <BiDish />
+                        Side Dish
+                      </span>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </>
+          )}
+
+          {!HAS_PRODUCT && (
+            <>
+              <h1 id={styles._error_theres_no_data_to_show}>
+                Something went wrong, Cannot show products.
+              </h1>
+              <button
+                id={styles._error_theres_no_data_to_show_BUTTON}
+                onClick={() => router.reload()}
+              >
+                Reload the page
+              </button>
+            </>
+          )}
         </Container>
       </div>
 
