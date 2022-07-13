2ca8b3a57aad646b55e155eabec8a43b79bbd0ea
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-13 17:15:18 +0800

    Added env variable to vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    dbString:
-      "mongodb+srv://devonly:devonly@cluster0.z6mkq.mongodb.net/?retryWrites=true&w=majority",
-  },
 };
 
 module.exports = nextConfig;
